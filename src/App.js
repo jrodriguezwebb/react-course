9d3e67551e7b285e0b701f88e8fac5d3b34eb312
Jesus Rodriguez <jesus.rodriguez@kbc.be>
2020-03-04 17:48:43 +0100

    scratch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+
+class Title extends Component {
+  render() {
+  return <h1>{this.props.text}</h1>
+  }
+}
+
+Title.defaultProps = {
+  text: 'Titulo por defecto'
+}
+
+class ContadorNumero extends Component {
+  render() {
+  return <span>{this.props.numero}</span>
+  }
+}
+
+class Contador extends Component {
+  state = { contador: this.props.contadorInicial }
+  // Old way
+  /* constructor(props) {
+    super(props)
+    this.state = { contador: 1 }
+  } */
+  constructor(props) {
+    super(props);
+    setInterval(() => {
+      this.setState({ contador: this.state.contador + 1 })
+    }, 1000);
+  }
+
+  render() {
+    return <ContadorNumero numero={this.state.contador} />
+  }
+}
+
+Contador.defaultProps = {
+  contadorInicial: 0,
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title text="Otro titulo desde props" />
+      <Contador contadorInicial={100} />
     </div>
   );
 }
